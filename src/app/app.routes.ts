b2c5415c2f546d90aa4c92094c98c19905f8a0aa
vnpugh <vnp19@yahoo.com>
2024-03-16 12:40:22 -0500

    added homecomponent route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        // lazily load the HomeComponent
+        loadComponent: () => import('./features/home/home.component').then(c => c.HomeComponent)
+    },
+];
