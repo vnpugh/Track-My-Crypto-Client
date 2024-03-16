647d46d090aa81dfd977a461aa5045c085fc6150
vnpugh <vnp19@yahoo.com>
2024-03-16 08:11:29 -0500

    Commiting updates to files

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { Routes, RouterModule } from "@angular/router";
+
+
+// Define project routes
+const routes: Routes = [
+    { path: '', component: HomeComponent, title: 'Home' }
+  ];
+
+  export class AppModule {
+    static withRoot(): any {
+      return {
+        ngModule: AppModule,
+        providers: [],
+        imports: [
+          BrowserModule,
+          RouterModule.forRoot(routes)
+        ],
+        bootstrap: [AppComponent] 
+      };
+    }
+  }
+
