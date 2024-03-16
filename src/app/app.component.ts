ac2b601eefd2ff07dea2c58fee9ca856bd508aa6
vnpugh <vnp19@yahoo.com>
2024-03-16 11:31:10 -0500

    updated app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./home/home.component";
+import { HomeComponent } from './features/home/home.component';
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HomeComponent]
+    imports: [RouterOutlet, HomeComponent,]
 })
 export class AppComponent {
   title = 'track-my-crypto-client';
